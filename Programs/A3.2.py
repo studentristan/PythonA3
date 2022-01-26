c1d313e25e5735f31fe91cb7efcf850db14cef54
studentristan <54915507+studentristan@users.noreply.github.com>
2022-01-25 21:24:55 -0500

    Add files via upload

---
+++ Programs/A3.2.py
@@ -0,0 +1,19 @@
+#assignment 3.2
+#Tristan Werden
+#Todo: None
+
+try :
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+except :
+    print("Error, please enter numeric input")
+    quit()
+
+total = hours * rate
+
+if hours > 40 :
+    overtime = hours - 40
+    overtimePay = overtime * (rate * .5)
+    total = total + overtimePay
+
+print(total)
